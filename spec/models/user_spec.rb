aa49aea9ed70dcddca35c56d7688afc0e23a8208
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-06-13 10:20:29 +0300

    User is able to create account and login

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, type: :model do
   describe 'creation' do
     it 'can be created' do
       user = User.create(username:"testuser", password:"asdf",password_confimation: "asdf")
-    
+      expect (user).to be_valid
     end
-  
   end
+  
 end
